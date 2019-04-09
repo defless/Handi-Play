bcaaf064c81acbd43b970838b1af1802f7274151
Defless <simon.deflesschouwer@mmibordeaux.com>
2019-04-09 17:51:54 +0200

    Mise en place

---
+++ Assets/CalculatorManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculatorManager : MonoBehaviour
+{
+	public float m_a = 0f;
+	public float m_b = 0f;
+	public float m_result = 0f;
+
+	public enum OperationType {ADD, SUB, MULT, DIV};
+	public OperationType m_currentOperation = OperationType.ADD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+      
+		if (m_currentOperation == OperationType.ADD)
+		{
+			m_result = Add(m_a, m_b);
+		}
+		if (m_currentOperation == OperationType.SUB)
+		{
+			m_result = Sub(m_a, m_b);
+		}
+		if (m_currentOperation == OperationType.MULT)
+		{
+			m_result = Mult(m_a, m_b);
+		}
+		if (m_currentOperation == OperationType.DIV)
+		{
+			m_result = Div(m_a, m_b);
+		}
+
+    }
+    float Add(float x, float y)
+    {
+	return x + y; 
+	}
+	float Sub(float x, float y)
+    {
+	return x - y; 
+	}
+	float Mult(float x, float y)
+    {
+	return x * y; 
+	}
+	float Div(float x, float y)
+    {
+	return x / y; 
+	}
+}
---
+++ Assets/PLayerBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerBehaviour : MonoBehaviour
+{
+	public float m_speed = 100f;
+	public Rigidbody2D m_rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    	if (Input.GetAxis("Horizontal") < 0f)
+    	{
+			m_rb2D.MovePosition(m_rb2D.position + Time.fixedDeltaTime * m_speed * Vector2.left);
+		}
+		if (Input.GetAxis("Horizontal") > 0f)
+    	{
+			m_rb2D.MovePosition(m_rb2D.position + Time.fixedDeltaTime * m_speed * Vector2.right);
+		}  
+		if (Input.GetAxis("Vertical") > 0f)
+    	{
+			m_rb2D.MovePosition(m_rb2D.position + Time.fixedDeltaTime * m_speed * Vector2.up);
+		}  
+		if (Input.GetAxis("Vertical") < 0f)
+    	{
+			m_rb2D.MovePosition(m_rb2D.position + Time.fixedDeltaTime * m_speed * Vector2.down);
+		}    
+    }
+}
